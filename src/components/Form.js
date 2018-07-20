47af2ca9fd9670ccf05f7af6b53d342f901df35d
GakwandiJoshua <gakwandijoshua@gmail.com>
2018-07-20 07:18:02 +0200

    dynamicLogin

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,26 +8,80 @@ import {
 } from 'react-native';
 
 export default class Form extends Component<{}> {
+  constructor(props){
+    super(props)
+    this.state={
+      username:'',
+      email:'', 
+      password:''       
+    }
+  }
+  
+  userRegister = () =>{
+    //alert('ok'); // version 0.48
+    
+    const {username} = this.state;
+    const {email} = this.state;
+    const {password} = this.state;
+    
+    
+    fetch('http://192.168.137.1:8081/api/clients', {
+      method: 'post',
+      header:{
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify({
+        username: 'username',
+        email: 'email',
+        password: 'password'
+        
+      })
+      
+    })
+    .then((response) => response.json())
+      .then((responseJson) =>{
+        alert(responseJson);
+      })
+      .catch((error)=>{
+        console.error(error);
+    });
+    
+  }
   render() {
     return (
         <View style={styles.container}>
+          {this.props.type=='Signup' &&
+            <TextInput style={styles.inputBox}
+              underlineColorAndroid='rgba(0,0,0,0)' 
+              placeholder="Username"
+              placeholderTextColor ="#ffffff"
+              selectionColor="#ffffff"
+              onChangeText={username => this.setState({username})}
+              onSubmitEditing={()=> this.email.focus()}
+              />
+          }
           <TextInput style={styles.inputBox}
               underlineColorAndroid='rgba(0,0,0,0)' 
               placeholder="Email adress..."
               placeholderTextColor ="#ffffff"
               selectionColor="#ffffff"
               keyboardType="email-address"
+              onChangeText= {email => this.setState({email})}
               onSubmitEditing={()=> this.password.focus()}
+              ref={(email)=> this.email = email}
               />
           <TextInput style={styles.inputBox}
               underlineColorAndroid='rgba(0,0,0,0)' 
               placeholder="Password"
               secureTextEntry={true}
               placeholderTextColor ="#fff"
+              selectionColor="#fff"
+              onChangeText= {password => this.setState({password})}
               ref={(input) => this.password = input}
-              />  
+              /> 
           <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>{this.props.type}</Text>
+            <Text style={styles.buttonText} onPress={this.userRegister}>{this.props.type}</Text>
           </TouchableOpacity> 
         </View>
     );
